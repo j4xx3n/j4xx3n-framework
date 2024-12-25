31c94af96b8ea17587a645fd1cd43b819aa3293e
Kali Linux <azureuser@kali.m1nv4shmknpubjj0t1pkqkreih.gx.internal.cloudapp.net>
2024-12-25 01:35:50 +0000

    j4xx3n

--- recon.sh
+++ recon.sh
@@ -31,27 +31,32 @@ done
 # subdomain enumberation with single domain
 if [ -n "$domain" ]; then
   echo $domain | anew $output/domain.txt
-  echo $domain | httpx-toolkit -td -ip -sc -cl -server | anew $output/domain-httpx.txt
+  echo $domain | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/domain-httpx.txt
   subfinder -d $domain | anew $output/subdomain.txt
-  cat $output/subdomain.txt | httpx-toolkit -td -ip -sc -cl -server |anew $output/subdomain-httpx.txt
+  cat $output/subdomain.txt | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/subdomain-httpx.txt
 fi
 
 
 # subdomain enumberation with multiple domains
 if [ -n "$list" ]; then
   cat $list | tee -a $output/domain.txt
-  cat $list | httpx-toolkit -td -ip -sc -cl -server | anew $output/domain-httpx.txt
+  cat $list | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/domain-httpx.txt
   subfinder -dL $list | anew $output/subdomain.txt
-  cat $output/subdomain.txt | httpx-toolkit -td -ip -sc -cl -server | anew $output/subdomain-httpx.txt
+  cat $output/subdomain.txt | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/subdomain-httpx.txt
 fi
 
 # directory enumeration
 cat $output/subdomain-httpx.txt | cut -d ' ' -f 1,2 | grep 200 | cut -d ' ' -f 1 | anew $output/subdomain-200.txt
 cat $output/subdomain-httpx.txt | cut -d ' ' -f 1 | hakrawler | anew $output/subdomain-crawl.txt
 cat $output/subdomain-httpx.txt | cut -d ' ' -f 1 |  waybackurls | anew $output/subdomain-crawl.txt
+cat $output/subdomain-httpx.txt | cut -d ' ' -f 1 | katana -u subdomains_alive.txt -d 5 waybackarchive,commoncrawl,alienvault -kf -jc -fx -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg | anew $output/subdomain-crawl.txt
 
-# vulnerability enumeration
+# vulnerable endpoint enumeration
 mkdir $output/GF
-cat $output/subdomain-crawl.txt | gf lfi | anew $output/GF/lfi
-cat $output/subdomain-crawl.txt | gf xss | anew $output/GF/xss
-cat $output/subdomain-crawl.txt | gf idor | anew $output/GF/idor
+
+
+cat $output/subdomain-crawl.txt | gf lfi | anew $output/GF/lfi # LFI enumeration
+cat $output/subdomain-crawl.txt | grep -E '.php|.asp|.aspx|.jspx|.jsp' | grep '=' | anew $output/GF/ci # command injection enumeration
+
+#cat $output/subdomain-crawl.txt | gf xss | anew $output/GF/xss
+#cat $output/subdomain-crawl.txt | gf idor | anew $output/GF/idor
