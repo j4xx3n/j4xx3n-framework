e0f01a6f3fcba58d3b8787cca02e0a17b5cd9672
Kali Linux <azureuser@kali.m1nv4shmknpubjj0t1pkqkreih.gx.internal.cloudapp.net>
2024-12-25 03:39:55 +0000

    j4xx3n

--- recon.sh
+++ recon.sh
@@ -30,33 +30,47 @@ done
 
 # subdomain enumberation with single domain
 if [ -n "$domain" ]; then
-  echo $domain | anew $output/domain.txt
-  echo $domain | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/domain-httpx.txt
-  subfinder -d $domain | anew $output/subdomain.txt
-  cat $output/subdomain.txt | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/subdomain-httpx.txt
+  # Make a directory for output
+  mkdir -p $output/Recon
+  echo $domain | anew $output/Recon/domain.txt
+  echo $domain | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/Recon/domain-httpx.txt
+  subfinder -d $domain | anew $output/Recon/subdomain.txt
+  cat $output/Recon/subdomain.txt | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/Recon/subdomain-httpx.txt
+
+  # directory enumeration
+  cat $output/Recon/subdomain-httpx.txt | cut -d ' ' -f 1,2 | grep 200 | cut -d ' ' -f 1 | anew $output/Recon/subdomain-200.txt
+  cat $output/Recon/subdomain-httpx.txt | cut -d ' ' -f 1 | hakrawler | anew $output/Recon/subdomain-crawl.txt
+  cat $output/Recon/subdomain-httpx.txt | cut -d ' ' -f 1 |  waybackurls | anew $output/Recon/subdomain-crawl.txt
+  cat $output/Recon/subdomain-httpx.txt | cut -d ' ' -f 1 | katana -u subdomains_alive.txt -d 5 waybackarchive,commoncrawl,alienvault -kf -jc -fx -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg | anew $output/Recon/subdomain-crawl.txt
+
+  # vulnerable endpoint enumeration
+  mkdir $output/Recon/GF
+  cat $output/Recon/subdomain-crawl.txt | gf lfi | anew $output/Recon/GF/lfi # LFI enumeration
+  cat $output/Recon/subdomain-crawl.txt | grep -E '.php|.asp|.aspx|.jspx|.jsp' | grep '=' | anew $output/Recon/GF/ci # command injection enumeration
+
+
 fi
 
 
 # subdomain enumberation with multiple domains
 if [ -n "$list" ]; then
-  cat $list | tee -a $output/domain.txt
-  cat $list | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/domain-httpx.txt
-  subfinder -dL $list | anew $output/subdomain.txt
-  cat $output/subdomain.txt | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/subdomain-httpx.txt
+  cat $list | cut -d '.' -f 1 | while read domainLoop; do
+  # Make a directory for each domain
+  mkdir -p $output/$domainLoop
+  mkdir -p $output/$domainLoop/Recon
+  cat $list | grep $domainLoop | anew $output/$domainLoop/Recon/domain.txt
+  cat $list | grep $domainLoop | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/$domainLoop/Recon/domain-httpx.txt
+  cat $list | grep $domainLoop | subfinder | anew $output/$domainLoop/Recon/subdomain.txt
+  cat $output/$domainLoop/Recon/subdomain.txt | httpx-toolkit -td -ip -sc -cl -server -ports 80,443,8080,8000,8888 -threads 200 | anew $output/$domainLoop/Recon/subdomain-httpx.txt
+  # directory enumeration
+  cat $output/$domainLoop/Recon/subdomain-httpx.txt | cut -d ' ' -f 1,2 | grep 200 | cut -d ' ' -f 1 | anew $output/$domainLoop/Recon/subdomain-200.txt
+  cat $output/$domainLoop/Recon/subdomain-httpx.txt | cut -d ' ' -f 1 | hakrawler | anew $output/$domainLoop/Recon/subdomain-crawl.txt
+  cat $output/$domainLoop/Recon/subdomain-httpx.txt | cut -d ' ' -f 1 |  waybackurls | anew $output/$domainLoop/Recon/subdomain-crawl.txt
+  cat $output/$domainLoop/Recon/subdomain-httpx.txt | cut -d ' ' -f 1 | katana -u subdomains_alive.txt -d 5 waybackarchive,commoncrawl,alienvault -kf -jc -fx -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg | anew $output/$domainLoop/Recon/subdomain-crawl.txt
+  # vulnerable endpoint enumeration
+  mkdir -p $output/$domainLoop/Recon/GF
+  cat $output/$domainLoop/Recon/subdomain-crawl.txt | gf lfi | anew $output/$domainLoop/Recon/GF/lfi # LFI enumeration
+  cat $output/$domainLoop/Recon/subdomain-crawl.txt | grep -E '.php|.asp|.aspx|.jspx|.jsp' | grep '=' | anew $output/$domainLoop/Recon/GF/ci; done
+
 fi
 
-# directory enumeration
-cat $output/subdomain-httpx.txt | cut -d ' ' -f 1,2 | grep 200 | cut -d ' ' -f 1 | anew $output/subdomain-200.txt
-cat $output/subdomain-httpx.txt | cut -d ' ' -f 1 | hakrawler | anew $output/subdomain-crawl.txt
-cat $output/subdomain-httpx.txt | cut -d ' ' -f 1 |  waybackurls | anew $output/subdomain-crawl.txt
-cat $output/subdomain-httpx.txt | cut -d ' ' -f 1 | katana -u subdomains_alive.txt -d 5 waybackarchive,commoncrawl,alienvault -kf -jc -fx -ef woff,css,png,svg,jpg,woff2,jpeg,gif,svg | anew $output/subdomain-crawl.txt
-
-# vulnerable endpoint enumeration
-mkdir $output/GF
-
-
-cat $output/subdomain-crawl.txt | gf lfi | anew $output/GF/lfi # LFI enumeration
-cat $output/subdomain-crawl.txt | grep -E '.php|.asp|.aspx|.jspx|.jsp' | grep '=' | anew $output/GF/ci # command injection enumeration
-
-#cat $output/subdomain-crawl.txt | gf xss | anew $output/GF/xss
-#cat $output/subdomain-crawl.txt | gf idor | anew $output/GF/idor
