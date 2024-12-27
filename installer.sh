12775d537d4a2b7136d9dad5fbe51597d7a422ec
j4xx3n <quinnelko14@outlook.com>
2024-12-27 09:18:43 -0500

    j4xx3n

--- installer.sh
+++ installer.sh
@@ -1,24 +1,24 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git curl wget python3 python3-pip golang
 go install -v github.com/tomnomnom/anew@latest
-sudo mv anew /usr/bin
+sudo mv ~/go/bin/anew /usr/bin
 sudo apt install httpx-toolkit
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
-sudo mv subfinder /usr/bin
+sudo mv ~/go/bin/subfinder /usr/bin
 go install github.com/hakluke/hakrawler@latest
-sudo mv hakrawler /usr/bin
+sudo mv ~/go/bin/hakrawler /usr/bin
 go install github.com/tomnomnom/waybackurls@latest
-sudo mv waybackurls /usr/bin
+sudo mv ~/go/bin/waybackurls /usr/bin
 go install github.com/projectdiscovery/katana/cmd/katana@latest
-sudo mv katana /usrm -rf gf_patternsr/bin
+sudo mv ~/go/bin/katana /usr/bin
 go install github.com/tomnomnom/gf@latest
-sudo mv gf /usr/bin
+sudo mv gf ~/go/bin//usr/bin
 mkdir -p ~/.gf
-git clone https://github.com/Sherlock297/gf_patterns.git
+#git clone https://github.com/Sherlock297/gf_patterns.git
 mv gf_patterns/*.json ~/.gf
 rm -rf gf_patterns
 sudo apt install cmake -y
-git clone https://github.com/ameenmaali/urldedupe.git
+#git clone https://github.com/ameenmaali/urldedupe.git
 cd urldedupe
 cmake CMakeLists.txt
 make
